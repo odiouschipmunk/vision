9d3636c7073b96dca89a14da58126434c86640d9
odiouschipmunk <odiouschipmunk@gmail.com>
2024-12-16 15:49:43 -0500

    better working w/main classes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import mainFunctions as mf
+
 def main():
-    import csv
-    import ast
-    frames=[]
-    with open('output/final.csv', 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            frames.append(row)
-    #goes through frame --> player1 --> first keypoint --> x
-    print(ast.literal_eval(frames[1][1])[0][0])
+    player1_pos, player2_pos = mf.read_player_positions()
+    mf.create_heatmap(player1_pos, player2_pos)
+    player1_pos = player1_pos[0]
+    player2_pos = player2_pos[0]
+    reference_points_3d = mf.load_reference_points()
+    #reference points in top left corner, top right corner, bottom right corner, bottom left corner, T point, left bottom of the service box, right bottom of the service box, left of tin, right of tin, left of the service line, right of the service line, left of the top line of the front court, right of the top line of the front court
+    reference_points=mf.read_reference_points()
+    print(reference_points)
+
 if __name__ == "__main__":
     main()
